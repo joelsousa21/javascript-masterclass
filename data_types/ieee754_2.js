cb87ef3d25e4a3c24e49125676aabefce5eb27aa
joelsousa21 <joelsousa21@outlook.com>
2025-01-06 09:42:22 -0300

    add an examples about ieee754

---
+++ data_types/ieee754_2.js
@@ -0,0 +1,18 @@
+1/0;
+Math.pow(10, 1000);
+Number.MAX_VALUE * 2;
+Math.log(0);
+-Number.MAX_VALUE * 2;
+
+// > 1/0;
+// Infinity
+// > Math.pow(10, 1000)
+// Infinity
+// > Number.MAX_VALUE
+// 1.7976931348623157e+308
+// > Number.MAX_VALUE * 2
+// Infinity
+// > -Number.MAX_VALUE * 2
+// -Infinity
+// > Math.log(0)
+// -Infinity
